f3dce1367de7e1445e4f4016cb89b209e18d35a3
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2024-03-20 00:20:11 +0600

    Basic Window

--- src/game/App.java
+++ src/game/App.java
@@ -1,6 +1,17 @@
 package game;
+
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JFrame window = new JFrame();
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("Chronicles Of Ranoa");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
